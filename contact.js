8dcc9eb2f2cea7576bc67643b2f8eb65d34a5427
delicedecerise <67659990+delicedecerise@users.noreply.github.com>
2020-11-22 18:50:01 +0100

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,84 @@
+var form = document.querySelector("form");
+
+var _nom = document.getElementById("nom"); var b_nom = false;
+var _prenom = document.getElementById("prenom"); var b_prenom = false;
+var _courriel = document.getElementById("courriel"); var b_courriel = false;
+var _message = document.getElementById("message"); var b_message = false;
+
+// b_*** boolean veriyfing if a value is okay
+
+var aidenom = document.getElementById("aidenom");
+var aideprenom = document.getElementById("aideprenom");
+var aidecourriel = document.getElementById("aidecourriel");
+var aidemessage = document.getElementById("aidemessage");
+
+var b = true;
+
+var regexMdpChiffre = /\d+/;
+var regexMdpLMinusc = /[a-z]/;
+var regexMdpLMajusc = /[A-Z]/;
+var regexCourriel = /^(([^<>()[]\.,;:s@]+(.[^<>()[]\.,;:s@]+)*)|(.+))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
+var regexSpecialChar = /[&"#{([\\_`|=°@+*/.<>?,;:!§µ¤$£^²~¨%)}\d]/;
+
+_nom.addEventListener("blur", function (e) {
+  if (regexSpecialChar.test(_nom.value)) {
+    b_nom = false;
+    aidenom.textContent = "votre nom ne doit contenir aucun caractère spécial.";
+  }
+  else {
+    b_nom = true;
+    aidenom.textContent = "";
+  }
+});
+
+_prenom.addEventListener("blur", function (e) {
+  if (regexSpecialChar.test(_prenom.value)) {
+    b_prenom = false;
+    aideprenom.textContent = "votre prénom ne doit contenir aucun caractère spécial.";
+  }
+  else {
+    b_prenom = true;
+    aideprenom.textContent = "";
+  }
+});
+
+_courriel.addEventListener("blur", function (e) {
+  if (!regexCourriel.test(_courriel.value)) {
+    b_courriel = false;
+    aidecourriel.textContent = "votre courriel est incorrect.";
+  }
+  else {
+    b_courriel = true;
+    aidecourriel.textContent = "";
+  }
+});
+
+_message.addEventListener("blur", function (e)){
+  if(_message.value.length == 0){
+    b_message = false;
+    aidemessage.textContent = "Un message vide ne sera pas envoyer";
+  } else {
+    b_message = true;
+    aidemessage.textContent = "";
+  }
+}
+
+
+
+// Affichage de toutes les données saisies ou choisies dans la console
+form.addEventListener("submit", function (e) {
+    console.log("Nom : " + form.elements.nom.value +
+      "\nPrénom : " + form.elements.prenom.value +
+      "\nEmail : " + form.elements.courriel.value +
+      "\nMessage : " + form.elements.message.value);
+
+    console.log(b_nom+" "+b_prenom+" "+b_courriel+" "+b_mdp+" "+b_confmdp+" "+b_message);
+
+    if(!(b_nom && b_prenom && b_courrie && b_message)){
+      console.log("Envoie annulé !");
+      e.preventDefault();
+    }
+    else{
+      console.log("Envoie effectué !");
+    }
+});
---
+++ contact.php
@@ -0,0 +1,82 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Contact</title>
+
+  <link rel="stylesheet" type="text/css" href="sign.css">
+
+  <!-- BOOTSTRAP CSS | STYLE CSS FILE FOR HEADER AND FOOTER -->
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+</head>
+<body>
+  <div class="d-flex w-100 h-100 p-3 mx-auto flex-column">
+
+    <?php require_once 'header.php'; ?> <!-- HEADER -->
+
+    <!-- MAIN BODY -->
+
+		<div class="container">
+        <div class="py-5 text-center">
+            <h2>Contactez-nous</h2>
+            <p class="lead">Devrions-nous écrire un mot ici ?</p>
+        </div>
+
+        <div class="row p-3 mb-3">
+          <div class="col-md-8 p-3 order-md-1 mx-auto">
+              <form class="" method="post" action="bdd/signupcontrol.php">
+
+                <div class="row">
+
+                  <div class="col-md-6 mb-3">
+                    <label for="nom">Nom</label>
+                    <input type="text" class="nc_form_control" name="nom" id="nom" placeholder="Nom" value="" required>
+                    <span class="nc_error" id="aidenom"></span>
+                  </div>
+
+                  <div class="col-md-6 mb-3">
+                      <label for="prenom">Prénom</label>
+                      <input type="text" class="nc_form_control" name="prenom" id="prenom" placeholder="Prénom" value="" required>
+                      <span class="nc_error" id="aideprenom"></span>
+                  </div>
+
+                </div>
+
+                <div class="mb-3">
+                    <label for="courriel">E-mail</label>
+                    <input type="email" class="nc_form_control" name="courriel" id="courriel" placeholder="exemple@domaine.fr" required>
+                    <span class="nc_error" id="aidecourriel"></span>
+                </div>
+
+                <div class="mb-3">
+                    <label for="message">Message</label><br>
+                    <textarea name="message" class="nc_form_control" id="message" placeholder="Saisissez votre message" required style="height:200px"></textarea>
+                    <span class="nc_error" id="aidemessage"></span>
+                </div>
+
+
+                <hr class="mb-4">
+
+                <button name="signup" class="btn btn-primary nc_btn_block btn-lg" type="submit">Envoyer</button>
+            </form>
+        </div>
+      </div>
+    </div>
+
+    <!-- END MAIN BODY -->
+
+    <?php require_once 'footer.php'; ?> <!-- FOOTER -->
+
+  </div>
+
+  <script src="contact.js" type="text/javascript"></script>
+  <script src="js/jquery.js" type="text/javascript"></script>
+  <script src="js/bootstrap.js" type="text/javascript"></script>
+
+</body>
+</html>
